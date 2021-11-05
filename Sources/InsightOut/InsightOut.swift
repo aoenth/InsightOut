bb6130a4f67bc779a152da6309364d2f1a2ce25d
Kevin Peng <peng.kevin@me.com>
2021-11-05 19:22:54 -0400

    Add basic data types

--- Sources/InsightOut/InsightOut.swift
+++ Sources/InsightOut/InsightOut.swift
@@ -1,6 +1,14 @@
-public struct InsightOut {
-    public private(set) var text = "Hello, World!"
+import Foundation
+public enum Mood {
+    case happy
+}
 
-    public init() {
-    }
+public enum Label {
+    case family
+}
+
+public struct MoodEntry {
+    let time: Date
+    let mood: Mood
+    let label: Label
 }
