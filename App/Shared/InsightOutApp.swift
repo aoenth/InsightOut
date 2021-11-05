bf226325bd759b63ea98c3a961a6cd4d5d616029
Kevin Peng <peng.kevin@me.com>
2021-11-05 18:27:01 -0400

    Add Xcode project file

---
+++ App/Shared/InsightOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightOutApp.swift
+//  Shared
+//
+//  Created by Kevin Peng on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct InsightOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
