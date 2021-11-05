f09cf70d8f8f94d28c0a16d9d4ca6c227d94b409
Kevin Peng <peng.kevin@me.com>
2021-11-05 18:21:10 -0400

    Add Swift package files

---
+++ Tests/InsightOutTests/InsightOutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import InsightOut
+
+final class InsightOutTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(InsightOut().text, "Hello, World!")
+    }
+}
