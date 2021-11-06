3f2a098eac485c66e69373d9e8f126517b1a5a31
Kevin Peng <peng.kevin@me.com>
2021-11-06 17:05:00 -0400

    Add test

---
+++ Tests/InsightOutTests/MoodEntryRepositoryTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import InsightOut
+
+final class MoodEntryRepositoryTests: XCTestCase {
+
+    func test_init() {
+
+    }
+
+    func test_persistence() throws {
+
+    }
+}
