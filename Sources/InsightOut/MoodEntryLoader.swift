50ad29b663814e09fea1be95589dcadbe7b62513
umpusten <2965914+UMPUSTEN@users.noreply.github.com>
2021-11-06 21:44:08 +0100

    first implementation of business logic fetching / saving

---
+++ Sources/InsightOut/MoodEntryLoader.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marc Gidaszewski on 06.11.21.
+//
+
+import Foundation
+
+protocol MoodEntryLoader {
+    func moods(forDate: Date) -> [MoodEntry]
+    func moods(forWeekStarting: Date) -> [Date: [MoodEntry]]
+    func moods(startDate: Date, endDate: Date) -> [Date: [MoodEntry]]
+    func saveMood(_ mood: Mood, date: Date, label: Label)
+}
