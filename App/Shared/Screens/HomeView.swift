88fc652779cc146597d98f9d13f123e72d14bee1
Kevin Peng <peng.kevin@me.com>
2021-11-07 14:30:18 -0500

    Add missing loader environment object

--- App/Shared/Screens/HomeView.swift
+++ App/Shared/Screens/HomeView.swift
@@ -59,5 +59,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(MoodEntryRepository(context: CoreDataStack.preview.context))
     }
 }
