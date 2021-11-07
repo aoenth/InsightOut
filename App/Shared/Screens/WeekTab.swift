90ffdaea6c6c3079b62fa18da5af78397827846a
Vincent <vincentbanguyen@gmail.com>
2021-11-07 14:48:58 -0600

    `

---
+++ App/Shared/Screens/WeekTab.swift
@@ -0,0 +1,20 @@
+//
+//  WeekTab.swift
+//  InsightOut
+//
+//  Created by Vincent Nguyen on 11/7/21.
+//
+
+import SwiftUI
+
+struct WeekTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeekTab_Previews: PreviewProvider {
+    static var previews: some View {
+        WeekTab()
+    }
+}
